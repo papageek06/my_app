750ae590b22a9af56032b775746f7ddfcef2355e
Timoshka177 <107550627+Timoshka177@users.noreply.github.com>
2025-02-03 11:06:36 +0100

    mise a jour C

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -20,7 +20,7 @@ class Article
 
     #[ORM\ManyToOne(inversedBy: 'articles')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?category $category = null;
+    private ?Category $category = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $picture = null;
@@ -62,12 +62,12 @@ class Article
         return $this;
     }
 
-    public function getCategory(): ?category
+    public function getCategory(): ?Category
     {
         return $this->category;
     }
 
-    public function setCategory(?category $category): static
+    public function setCategory(?Category $category): static
     {
         $this->category = $category;
 
